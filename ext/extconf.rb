077a3244524cbe9d90b3b6f2de066787f73009f0
Justin DeWind <dewind@atomicobject.com>
2010-01-13 13:07:33 -0500

    small update to extconf

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -6,8 +6,17 @@ $CFLAGS << ' ' + "-ObjC"
 $LDFLAGS << ' ' + "-lobjc -framework Foundation -framework Growl"
 $objs = %w(ruby_delegate.o real_growl_api.o)
 
-if(have_library("objc"))
-  extension_name = 'real_growl_api'
-  dir_config(extension_name)
-  create_makefile(extension_name)
+def growl_framework_paths
+  ["/Library/Frameworks/Growl.framework", "#{ENV['HOME']}/Library/Growl.framework"]
+end
+
+if(growl_framework_paths.any? {|x| File.exists?(x)})
+  message("checking for framework Growl... yes\n")
+  if(have_library("objc"))
+    extension_name = 'real_growl_api'
+    dir_config(extension_name)
+    create_makefile(extension_name)
+  end
+else
+  message("checking for framework Growl... no\n")
 end
