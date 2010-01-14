74605f68a4fb3dd77725cae0d8c5bdb333ed5704
Justin DeWind <dewind@atomicobject.com>
2010-01-14 08:05:45 -0500

    fixing memory leak in RubyDelegate

--- ext/ruby_delegate.m
+++ ext/ruby_delegate.m
@@ -3,12 +3,23 @@
 @implementation RubyDelegate
 @synthesize applicationName;
 
--(NSDictionary *)registrationDictionaryForGrowl {
-  if(applicationName == nil) {
+-(id)init {
+  if(self = [super init]) {
     applicationName = @"RealGrowl";
+    return self;
   }
+  
+  return nil;
+}
+
+-(NSDictionary *)registrationDictionaryForGrowl {
   NSArray *notifications = [NSArray arrayWithObject:REAL_GROWL_NOTIFICATION];
   return [NSDictionary dictionaryWithObjectsAndKeys: [self applicationName], GROWL_APP_ID, [self applicationName], GROWL_APP_NAME, notifications, GROWL_NOTIFICATIONS_ALL, nil];
+}
+
+-(void)dealloc {
+  [applicationName release];
+  [super dealloc];
 }
 
 @end
