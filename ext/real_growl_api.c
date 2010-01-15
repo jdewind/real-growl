01a6087ef90728c339a715ab727d9530f13d5a3c
Justin DeWind <dewind@atomicobject.com>
2010-01-14 21:24:12 -0500

    added function for creating auto release pools

--- ext/real_growl_api.c
+++ ext/real_growl_api.c
@@ -3,61 +3,6 @@
 VALUE rb_mRealGrowl;
 VALUE rb_cRealGrowlApplication;
 
-VALUE
-classMethod_installed(VALUE self) {
-  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  BOOL installed = [GrowlApplicationBridge isGrowlInstalled];
-  [pool release];
-  
-  if(installed) {
-    return Qtrue;
-  } else {
-    return Qfalse;
-  }
-}
-
-VALUE
-classMethod_running(VALUE self) {
-  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  BOOL running = [GrowlApplicationBridge isGrowlRunning];
-  [pool release];
-  
-  if(running) {
-    return Qtrue;
-  } else {
-    return Qfalse;
-  }
-}
-
-VALUE
-method_notify(VALUE self, VALUE title, VALUE description, VALUE priority, VALUE sticky) {
-  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  
-  BOOL nsSticky = (sticky == Qtrue) ? YES : NO;
-  NSString *nsTitle = [NSString stringWithCString:STR2CSTR(title) encoding: NSASCIIStringEncoding];
-  NSString *nsDescription = [NSString stringWithCString:STR2CSTR(description) encoding: NSASCIIStringEncoding];
-  [GrowlApplicationBridge notifyWithTitle: nsTitle description: nsDescription notificationName: REAL_GROWL_NOTIFICATION iconData: nil priority: NUM2INT(priority) isSticky:nsSticky clickContext:nil];
-  
-  [pool release];
-  
-  return Qnil;
-}
-
-VALUE 
-method_init(VALUE self, VALUE applicationName) {
-  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-  id delegate = NULL;
-  
-  Data_Get_Struct(self, RubyDelegate, delegate);
-  
-  NSString *nsAppName = [NSString stringWithCString:STR2CSTR(applicationName) encoding: NSASCIIStringEncoding];
-  [delegate setApplicationName: nsAppName];
-  [GrowlApplicationBridge setGrowlDelegate: delegate];
-  
-  [pool release];
-  return self;
-}
-
 static void
 free_delegate(id delegate) {
   [delegate release];
@@ -68,10 +13,68 @@ alloc_delegate(VALUE klass) {
   return Data_Wrap_Struct(klass, 0, free_delegate, [[RubyDelegate alloc] init]);  
 }
 
+static NSAutoreleasePool*
+create_autorelease_pool() {
+  return [[NSAutoreleasePool alloc] init];
+}
+
+VALUE
+classMethod_installed(VALUE self) {
+  NSAutoreleasePool *pool = create_autorelease_pool();
+  BOOL installed = [GrowlApplicationBridge isGrowlInstalled];
+  [pool drain];
+  
+  if(installed) {
+    return Qtrue;
+  } else {
+    return Qfalse;
+  }
+}
+
+VALUE
+classMethod_running(VALUE self) {
+  NSAutoreleasePool *pool = create_autorelease_pool();
+  BOOL running = [GrowlApplicationBridge isGrowlRunning];
+  [pool drain];
+  
+  if(running) {
+    return Qtrue;
+  } else {
+    return Qfalse;
+  }
+}
+
+VALUE
+method_notify(VALUE self, VALUE title, VALUE description, VALUE priority, VALUE sticky) {
+  NSAutoreleasePool *pool = create_autorelease_pool();
+  
+  BOOL nsSticky = (sticky == Qtrue) ? YES : NO;
+  NSString *nsTitle = [NSString stringWithCString:STR2CSTR(title) encoding: NSASCIIStringEncoding];
+  NSString *nsDescription = [NSString stringWithCString:STR2CSTR(description) encoding: NSASCIIStringEncoding];
+  [GrowlApplicationBridge notifyWithTitle: nsTitle description: nsDescription notificationName: REAL_GROWL_NOTIFICATION iconData: nil priority: NUM2INT(priority) isSticky:nsSticky clickContext:nil];
+  
+  [pool drain];
+  
+  return Qnil;
+}
+
+VALUE 
+method_init(VALUE self, VALUE applicationName) {
+  NSAutoreleasePool *pool = create_autorelease_pool();
+  id delegate = NULL;  
+  
+  Data_Get_Struct(self, RubyDelegate, delegate);  
+  NSString *nsAppName = [NSString stringWithCString:STR2CSTR(applicationName) encoding: NSASCIIStringEncoding];
+  [delegate setApplicationName: nsAppName];
+  [GrowlApplicationBridge setGrowlDelegate: delegate];
+  
+  [pool drain];
+  
+  return self;
+}
 
 void
-Init_real_growl_api() {
-  
+Init_real_growl_api() {  
   rb_mRealGrowl = rb_define_module("RealGrowl");
   rb_define_singleton_method(rb_mRealGrowl, "installed?", classMethod_installed, 0);
   rb_define_singleton_method(rb_mRealGrowl, "running?", classMethod_running, 0);
