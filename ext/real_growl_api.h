b811b6c454b718301f3ad9b9bf9fd3a17212d811
Justin DeWind <dewind@atomicobject.com>
2010-02-05 07:29:14 -0500

    putting static declarations in real_growl_api.h

--- ext/real_growl_api.h
+++ ext/real_growl_api.h
@@ -3,3 +3,9 @@
 #include <objc/runtime.h>
 #include <Growl/Growl.h>
 #include <Foundation/Foundation.h>
+
+static NSString* build_nsstring(VALUE string);
+static NSData* get_icon_data(VALUE pathOrUrl);
+static NSAutoreleasePool* create_autorelease_pool();
+static void free_delegate(id delegate);
+static VALUE alloc_delegate(VALUE klass);
