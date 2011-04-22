3002c9cece8d9ff4772e6dcd710f4e208df6f619
Justin DeWind <dewind@atomicobject.com>
2011-04-22 13:17:04 -0400

    only check for growl in system library. Removed check for objc since it is not consistent

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -7,16 +7,14 @@ $LDFLAGS << ' ' + "-lobjc -framework Foundation -framework Growl"
 $objs = %w(ruby_delegate.o real_growl_api.o)
 
 def growl_framework_paths
-  ["/Library/Frameworks/Growl.framework", "#{ENV['HOME']}/Library/Frameworks/Growl.framework"]
+  ["/Library/Frameworks/Growl.framework"]
 end
 
 if(growl_framework_paths.any? {|x| File.exists?(x)})
   message("checking for framework Growl... yes\n")
-  if(have_library("objc"))
-    extension_name = 'real_growl_api'
-    dir_config(extension_name)
-    create_makefile(extension_name)
-  end
+  extension_name = 'real_growl_api'
+  dir_config(extension_name)
+  create_makefile(extension_name)
 else
   message("checking for framework Growl... no\n")
 end
