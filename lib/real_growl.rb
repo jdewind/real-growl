5ab32bbfd4101297bf0862cadb34e33589e8da36
Justin DeWind <dewind@atomicobject.com>
2011-05-30 15:36:34 -0400

    Re-factored ruby implementation to use a module mixin

--- lib/real_growl.rb
+++ lib/real_growl.rb
@@ -1,38 +1,53 @@
 require 'real_growl_api'
 require 'pp'
 
-module Kernel
-  class<<self
-    attr_accessor :rg_sticky, :rg_priority, :rg_icon
+module RealGrowl    
+  module ClassMethods
+    attr_accessor :sticky, :priority, :icon
+    
+    def growl(*args)
+      self.priority ||= 0
+      self.sticky = true if self.sticky.nil?
+      icon = self.icon
+      click = nil
+
+      @__growl_app ||= RealGrowl::Application.new("RealGrowl")
+
+      if args.last.is_a?(Hash) and (args.last[:icon] or args.last[:click])
+        icon = args.last[:icon]
+        click = args.last[:click]
+        args.pop
+      end
+
+      if RealGrowl.running?
+        notifications = args.map { |x| x.pretty_inspect }
+        notifications.each do |x|
+          @__growl_app.notify(
+            :title        => 'rg', 
+            :description  => x, 
+            :priority     => self.priority, 
+            :sticky       => self.sticky, 
+            :icon         => icon
+          )
+        end
+      end
+      nil
+    end
   end
   
-  def rg(*args)
-    Kernel.rg_priority ||= 0
-    Kernel.rg_sticky = true if Kernel.rg_sticky.nil?
-    icon = Kernel.rg_icon
-    click = nil
-    
-    @__growl_app ||= RealGrowl::Application.new("RealGrowl")
-    
-    if args.last.is_a?(Hash) and (args.last[:icon] or args.last[:click])
-      icon = args.last[:icon]
-      click = args.last[:click]
-      args.pop
+  module InstanceMethods
+    def rg(*args)
+      RealGrowl.real_growl(*args)
     end
-    
-    if RealGrowl.running?
-      notifications = args.map { |x| x.pretty_inspect }
-      notifications.each do |x|
-        @__growl_app.notify(
-          :title        => 'rg', 
-          :description  => x, 
-          :priority     => Kernel.rg_priority, 
-          :sticky       => Kernel.rg_sticky, 
-          :icon         => icon
-        )
-      end
-    end
-        
-    nil
   end
+  
+  def self.included(receiver)
+    receiver.send :include, InstanceMethods
+  end
+  
+  extend self::ClassMethods
+end
+
+class Object  
+  include RealGrowl
 end
